691fb3040e056843572fe1507699902d8d8df071
Adam Samulski <a.samulski@op.pl>
2019-08-10 21:13:17 +0200

    header project & structure

--- new-site/src/layouts/Header.js
+++ new-site/src/layouts/Header.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import '../styles/Header.css'
+import header1 from '../images/brocoli.jpg';
+import header2 from '../images/grapefruit.jpg';
+import header3 from '../images/vegetables.jpg';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <>
+            <Switch>
+                <Route path="/" exact render={() => (<img src={header3} alt="vegetables" />)} />
+                <Route path="/products" render={() => (<img src={header2} alt="orange" />)} />
+                <Route path="/contact" render={() => (<img src={header1} alt="broccoli" />)} />
+                <Route path="/admin" render={() => (<img src={header1} alt="broccoli" />)} />
+                {/* na dole route do error 404 */}
+                <Route render={() => (<img src={header3} alt="vege" />)} />
+            </Switch>
+
+        </>
     )
 }
 
