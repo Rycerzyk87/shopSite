7b0df9b9a86647daebddc4f4da65e4aefa561793
Adam Samulski <a.samulski@op.pl>
2019-08-12 20:28:50 +0200

    form style

--- new-site/src/pages/ContactPage.js
+++ new-site/src/pages/ContactPage.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import '../styles/ContactPage.css';
 
+class ContactPage extends React.Component {
+    state = {
+        value: "",
+    }
+    handleChange = (e) => {
+        this.setState({
+            value: e.target.value
+        })
+    }
 
-const ContactPage = () => {
-    return (
-        <div>KONTAKT</div>
-    );
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({
+            value: ""
+        })
+    }
+    render() {
+        return (
+            <>
+                <div className="contact">
+                    <form onSubmit={this.handleSubmit}>
+                        <h3>Napisz do nas</h3>
+                        <textarea value={this.state.value} onChange={this.handleChange} placeholder="Zostaw nam wiadomość"></textarea>
+                        <button on>Wyślij</button>
+                    </form>
+                </div>
+            </>
+        );
+    }
 }
 
 export default ContactPage;
