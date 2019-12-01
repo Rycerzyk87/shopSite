11cca4b8c1aad0d0cdefe3b9cf96bac5e359a7c3
Adam Samulski <a.samulski@op.pl>
2019-12-01 19:56:42 +0100

    error page

--- new-site/README.md
+++ new-site/README.md
@@ -1,14 +1,12 @@
 ### Vegetables Shop
 
-This is my  vegetables shop site to train issues of React.js.
+This is my  vegetables shop, i made this site to train issues of React.js.
 
 I used react and react router dom.
 
-### Available Scripts
+### `npm start`
 
 In the project directory, you can run:
-
-### `npm start`
 
 Runs the app in the development mode.<br>
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
--- new-site/src/pages/ErrorPage.js
+++ new-site/src/pages/ErrorPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
-
+// import panic from '../images/panic';
 
 const ErrorPage = () => {
     return (
         <>
-            <div className="errorMessage">Wskazana strona nie istnieje ;-(</div>
+            <div className="errorMessage">Podana strona nie istnieje ;-(</div>
+            {/* <img src={panic} alt="don't panic" /> */}
             <a href="/" className="backButton">Powrót na stronę główną</a>
         </>
     );
